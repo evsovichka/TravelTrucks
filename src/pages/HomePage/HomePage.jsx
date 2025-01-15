dfd8d4c6c4bb22a9286add479858ad4825cadb95
Yevsovych Kateryna <159715045+evsovichka@users.noreply.github.com>
2025-01-15 23:54:32 +0200

    add homePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,17 @@
+import css from "./HomePage.module.css";
 export default function HomePage() {
-    return (
-        <>
-        <h1>Home page</h1></>
-    )
+  return (
+    <>
+      <div className={css.background}></div>
+      <div className={css.wrapper}>
+        <div className={css.textWrapper}>
+          <h1 className={css.title}>Campers of your dreams</h1>
+          <p className={css.text}>
+            You can find everything you want in our catalog
+          </p>
+        </div>
+        <button className={css.button}>View Now</button>
+      </div>
+    </>
+  );
 }
