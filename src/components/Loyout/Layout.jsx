3b8b69c2f95dd16ab32b54d7d96cfd725e9ad687
Yevsovych Kateryna <159715045+evsovichka@users.noreply.github.com>
2025-01-18 14:51:33 +0200

    add NotFoundPage

--- src/components/Loyout/Layout.jsx
+++ src/components/Loyout/Layout.jsx
@@ -2,12 +2,28 @@ import AppBar from "../AppBar/AppBar.jsx";
 import { Suspense } from "react";
 import css from "./Loyout.module.css";
 import SuspenseFallback from "../SuspenseFallback/SuspenseFallback.jsx";
+import { useLocation } from "react-router-dom";
+import clsx from "clsx";
 
 export default function Layout({ children }) {
+  const location = useLocation();
+
+  const routesWithAppBar = [
+    "/",
+    "/catalog",
+    "/catalog/orders",
+    "/catalog/bookingForm",
+  ];
+  const shouldShowAppBar =
+    routesWithAppBar.includes(location.pathname) ||
+    location.pathname.startsWith("/catalog/");
+
   return (
     <div>
-      <AppBar />
-      <main className={css.container}>
+      {shouldShowAppBar && <AppBar />}
+      <main
+        className={clsx(css.container, { [css.withAppBar]: shouldShowAppBar })}
+      >
         <Suspense fallback={<SuspenseFallback />}>{children}</Suspense>
       </main>
     </div>
--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,6 +1,21 @@
+import { useNavigate } from "react-router-dom";
+import css from "./NotFoundPage.module.css";
 export default function NotFoundPage() {
-    return (
-        <>
-        <h1>Not Found Page</h1></>
-    )
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/");
+  };
+  return (
+    <>
+      <div className={css.background}></div>
+      <div className={css.wrapper}>
+        <div className={css.wrapperText}>
+          <h1 className={css.title}>Oops! Page Not Found</h1>
+          <a className={css.link} onClick={handleClick}>
+            Please return to the homepage
+          </a>
+        </div>
+      </div>
+    </>
+  );
 }
